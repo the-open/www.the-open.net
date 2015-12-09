b21e9a8c2f3291af1ff97ee674f3c08ec5e6307a
michaelsnook <michael.snook@gmail.com>
2015-12-09 17:14:14 +0530

    something is working. (with maps and mapbox)

---
+++ _includes/orgs.js
@@ -0,0 +1,219 @@
+/* 'core': [
+  [45.42152, -75.69719], //leadnow
+  [38.90719, -77.03687], //moveon
+  [51.50735, -0.12775], //38degrees
+  [52.52, 13.40495], //campact
+  [-35.28199, 149.12868] //getup
+],
+'startup': [
+  [-41.28646, 174.77623], //actionstation
+  [53.3498, -6.2603], //uplift
+  [46.94797, 7.44744], //operationlibero
+  [52.22967, 21.01222], //akcjademokracja
+  [44.42676, 26.10253], //de-clic
+  [-26.2041, 28.0473], //amandla.mobi
+  [59.32932, 18.06858], //skiftet
+  [48.20817, 16.37381] //aufstehn
+
+],
+'prelaunch': [
+  [41.87241, 12.48022], //progressi
+  [4.71098, -74.07209], //unetuvoz
+  [31.76831, 35.2137] //zazim
+]
+*/
+
+var orgs = [
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-75.69719, 45.42152]
+    },
+    properties: {
+      title: 'Leadnow',
+      description: '',
+      category: 'core'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-77.03687, 38.90719]
+    },
+    properties: {
+      title: 'MoveOn',
+      description: '',
+      category: 'core'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-0.12775, 51.50735]
+    },
+    properties: {
+      title: '38 Degrees',
+      description: '',
+      category: 'core'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [13.40495, 52.52]
+    },
+    properties: {
+      title: 'Campact',
+      description: '',
+      category: 'core'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [149.12868, -35.28199]
+    },
+    properties: {
+      title: 'GetUp',
+      description: '',
+      category: 'core'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [174.77623, -41.28646]
+    },
+    properties: {
+      title: 'ActionStation',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-6.2603, 53.3498]
+    },
+    properties: {
+      title: 'Uplift',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [7.44744, 46.94797]
+    },
+    properties: {
+      title: 'Operation Libero',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [21.01222, 52.22967]
+    },
+    properties: {
+      title: 'Akcja Demokracja',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [26.10253, 44.42676]
+    },
+    properties: {
+      title: 'de-clic',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [28.0473,-26.2041]
+    },
+    properties: {
+      title: 'Amandla',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [18.06858, 59.32932]
+    },
+    properties: {
+      title: 'Skiftet',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [16.37381, 48.20817]
+    },
+    properties: {
+      title: 'Aufstehn',
+      description: '',
+      category: 'startup'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [12.48022, 41.87241]
+    },
+    properties: {
+      title: 'Progressi',
+      description: '',
+      category: 'prelaunch'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-74.07209, 4.71098]
+    },
+    properties: {
+      title: 'UneTuVoz',
+      description: '',
+      category: 'prelaunch'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [35.2137, 31.76831]
+    },
+    properties: {
+      title: 'Zazim',
+      description: '',
+      category: 'prelaunch'
+    }
+  }
+];
