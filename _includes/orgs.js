7f2a1ae7470a163f82c347f8df6754f3c7a64b29
michaelsnook <michael.snook@gmail.com>
2016-05-08 16:27:23 +0530

    clarify structure and edit data

--- _includes/orgs.js
+++ _includes/orgs.js
@@ -1,16 +1,13 @@
-var orgs = [
-  {% for org in site.data.orgs %}
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: {{ org.coordinates }}
-    },
-    properties: {
-      title: '{{ org.title }}',
-      description: '{% if org.logo %}<img src="{{ org.logo }}" alt="{{ org.title }}" /><br />{% endif %}{{ org.description }}',
-      category: '{{ org.category }}'
-    }
-  }{% if forloop.last %}{% else %},{% endif %}
-  {% endfor %}
-];
+{% for org in site.data.orgs %}{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: {{ org.coordinates }}
+  },
+  properties: {
+    title: '{{ org.title }}',
+    description: '{% if org.logo %}<img src="{{ org.logo }}" alt="{{ org.title }}" /><br />{% endif %}{{ org.description }}',
+    category: '{{ org.category }}'
+  }
+}{% if forloop.last == false %},{% endif %}
+{% endfor %}
